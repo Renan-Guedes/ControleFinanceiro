cbb21f23dd229397da1e5b531d5093aeaad71b07
Renan Guedes <renanguedesrdg@gmail.com>
2025-05-17 06:20:47 -0300

    feat(categoria): adiciona interfaces e implementações na camada application

---
+++ ControleFinanceiro.Application/Interfaces/ICategoriaUseCase.cs
@@ -0,0 +1,17 @@
+﻿using ControleFinanceiro.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleFinanceiro.Application.Interfaces;
+
+public interface ICategoriaUseCase
+{
+    List<Categoria> ListarTodas();
+    List<Categoria> ListarPorNome(string categoria);
+    void Criar(Categoria categoria);
+    void Atualizar(Categoria categoria);
+    void Deletar(int id);
+}
---
+++ ControleFinanceiro.Application/UseCases/CategoriaUseCase.cs
@@ -0,0 +1,33 @@
+﻿using ControleFinanceiro.Application.Interfaces;
+using ControleFinanceiro.Domain.Models;
+using ControleFinanceiro.Domain.Repositories;
+
+namespace ControleFinanceiro.Application.UseCases;
+
+public class CategoriaUseCase : ICategoriaUseCase
+{
+    private readonly ICategoriaRepository _repository;
+
+    public CategoriaUseCase(ICategoriaRepository repository)
+    {
+        _repository = repository;
+    }
+
+    #region Métodos de Alteração
+
+    public void Criar(Categoria categoria) => _repository.Criar(categoria);
+
+    public void Atualizar(Categoria categoria) => _repository.Atualizar(categoria);
+
+    public void Deletar(int id) => _repository.Deletar(id);
+
+    #endregion
+
+    #region Métodos de Pesquisa
+
+    public List<Categoria> ListarTodas() => _repository.Listar();
+
+    public List<Categoria> ListarPorNome(string categoria) => _repository.ListarPorNome(categoria);
+
+    #endregion
+}
