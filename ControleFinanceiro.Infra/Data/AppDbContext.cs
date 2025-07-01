8099635b90dfd2dc9c7cc7adc5420a0e98394879
Renan Guedes <renanguedesrdg@gmail.com>
2025-06-30 23:53:35 -0300

    chore(db): insere registros iniciais de Receita e Despesa via seed

--- ControleFinanceiro.Infra/Data/AppDbContext.cs
+++ ControleFinanceiro.Infra/Data/AppDbContext.cs
@@ -20,9 +20,32 @@ public class AppDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        // Categoria
         modelBuilder.Entity<CategoriaModel>().ToTable("Categoria");
+
+        // Transacao
         modelBuilder.Entity<TransacaoModel>().ToTable("Transacao");
+
+        // TipoTransacao
         modelBuilder.Entity<TipoTransacaoModel>().ToTable("TipoTransacao");
+        modelBuilder.Entity<TipoTransacaoModel>().HasData(
+            new TipoTransacaoModel 
+            { 
+                Id = 1, Nome = "Receita", 
+                DataInclusao = new DateTime(2025, 01, 01),
+                DataAtualizacao = null,
+                DataExclusao = null
+            },
+            new TipoTransacaoModel 
+            { 
+                Id = 2, Nome = "Despesa",
+                DataInclusao = new DateTime(2025, 01, 01),
+                DataAtualizacao = null,
+                DataExclusao = null
+            }
+        );
+
+        // Banco
         modelBuilder.Entity<BancoModel>().ToTable("Banco");
 
         base.OnModelCreating(modelBuilder);
